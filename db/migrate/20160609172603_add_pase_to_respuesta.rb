26f1b3b45ef714d6ce6311bc3aef48d7a94f461c
alfredo777 <jose_alfredo@cursa.me>
2016-06-12 20:07:42 -0500

    cambio en integer

--- db/migrate/20160609172603_add_pase_to_respuesta.rb
+++ db/migrate/20160609172603_add_pase_to_respuesta.rb
@@ -1,7 +1,7 @@
 class AddPaseToRespuesta < ActiveRecord::Migration
   def change
-    add_column :respuesta, :pase, :number, default: 0
-    add_column :respuesta, :respuesta, :number, default: 0
+    add_column :respuesta, :pase, :integer, default: 0
+    add_column :respuesta, :respuesta, :integer, default: 0
 
   end
 end
