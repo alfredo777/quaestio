1456d28e15cf3a59c67b71ab2218d603f027abba
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2016-10-28 14:48:09 -0500

    cambios en fechas

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Quaestio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Mexico City"
-    config.active_record.default_timezone = "Mexico City"
+    config.active_record.default_timezone = :local
 
     #heroku config:add TZ="Mexico City" -a agora-forms
 
@@ -26,7 +26,7 @@ module Quaestio
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
