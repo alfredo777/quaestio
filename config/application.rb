cf8ba0853d2fb56d1bfb670708e625eeecdc75c6
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2016-10-28 12:55:13 -0500

    timezone

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Quaestio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Mexico City"
-    config.active_record.default_timezone = :local
+    config.active_record.default_timezone = "Mexico City"
 
     #heroku config:add TZ="Mexico City" -a agora-forms
 
